416f61465ad0d9cdbd49712c4a63daf4ab7e6a97
Enrico Zangari <149686602+nricoz@users.noreply.github.com>
2024-04-18 13:49:09 -0400

    Add Files

---
+++ script.js
@@ -0,0 +1,12 @@
+image = document.getElementById('myImg2')
+      
+      Promise.all([
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+      ]).then(start)
+
+      function start() {
+        const image = await faceapi.bufferToImage(image)
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+        document.body.append(detections.length)
+      }
