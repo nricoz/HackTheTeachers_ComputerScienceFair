44227e1ad2e8eca765cf03566b550eebfe57d8a6
Enrico Zangari <149686602+nricoz@users.noreply.github.com>
2024-04-18 13:58:52 -0400

    Test start function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 image = document.getElementById('myImg2')
       
-      Promise.all([
-        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-      ]).then(start)
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start)
+function start() {
+  const image = await faceapi.bufferToImage(image)
+  const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+  document.body.append(detections.length)
+}
 
-      function start() {
-        const image = await faceapi.bufferToImage(image)
-        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-        document.body.append(detections.length)
-      }
+start()
+      
