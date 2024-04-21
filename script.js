b6b5e12cbd435f5cdb5326c3f1e75520e4d7880c
Enrico Zangari <149686602+nricoz@users.noreply.github.com>
2024-04-21 11:33:50 -0400

    new code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-pic = document.getElementById('myImg2');
-      
+const imageUpload = document.getElementById("imageUpload")
+
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start);
-function start() {
-  const image = await faceapi.bufferToImage(pic);
-  const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
-  document.body.append(detections.length);
+faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+faceapi.nets.ssMobilenetv1.loadFromUri('/models')
+]).then(start)
+
+function start(){
+  document.body.append("Loaded")
+  
 }
-      
